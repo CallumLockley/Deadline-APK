8bdcacf05802730d6bf8e545202a6bc8c2d9b38b
Callum Lockley <lockleycallum1@gmail.com>
2024-03-22 17:13:29 +0000

    Added background colour to main page.

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,27 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(
+        title: const Text('Deadlines'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[300],
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.lightBlueAccent, Colors.blue],
+          ),
+        ),
+        child: (const Center(
+          child: Text(
+            'Welcome to Deadlines',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+          ),
+        )),
+      ),
     );
   }
 }
